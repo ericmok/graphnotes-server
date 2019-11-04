21bf3935cbd5a654cdab88e2bfff9c241c9246da
Eric Mok <ericmok2@gmail.com>
2019-11-04 00:52:09 -0500

    Added connection to apollo server context. Cleaned up some prototyping code.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import * as fs from 'fs';
-import { ApolloServer, gql } from 'apollo-server';
+import { ApolloServer, gql, UserInputError } from 'apollo-server';
 import resolvers from './resolvers';
 import "reflect-metadata";
 import {createConnection} from "typeorm";
@@ -7,30 +7,18 @@ import {User} from "./entity/User";
 
 createConnection().then(async connection => {
 
-  console.log("Inserting a new user into the database...");
-  const user = new User();
-  user.firstName = "Timber";
-  user.lastName = "Saw";
-  user.age = 25;
-  await connection.manager.save(user);
-  console.log("Saved a new user with id: " + user.id);
-
-  console.log("Loading users from the database...");
-  const users = await connection.manager.find(User);
-  console.log("Loaded users: ", users);
-
-  console.log("Here you can setup and run express/koa/any other framework.");
-  const server = new ApolloServer({
-    typeDefs: gql(fs.readFileSync(__dirname.concat('/schema.graphql'), 'utf8')),
-    resolvers,
-    context: request => ({
-      ...request
-    })
-  });
-  
-  server.listen().then(({ url }) => {
-    console.log(`🚀 Server ready at ${url}`);
-  });
-  
+    const server = new ApolloServer({
+      typeDefs: gql(fs.readFileSync(__dirname.concat('/schema.graphql'), 'utf8')),
+      resolvers,
+      context: request => ({
+        ...request,
+        db: connection
+      })
+    });
+    
+    server.listen().then(({ url }) => {
+      console.log(`🚀 Server ready at ${url}`);
+    });
+    
 }).catch(error => console.log(error));
 

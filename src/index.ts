eaf7bdb2d5fa73180c405c010bb7dd140e3d725a
Eric Mok <ericmok2@gmail.com>
2019-11-03 23:53:40 -0500

    Added typeorm to project

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,36 @@
 import * as fs from 'fs';
 import { ApolloServer, gql } from 'apollo-server';
 import resolvers from './resolvers';
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+import {User} from "./entity/User";
 
-const server = new ApolloServer({
-  typeDefs: gql(fs.readFileSync(__dirname.concat('/schema.graphql'), 'utf8')),
-  resolvers,
-  context: request => ({
-    ...request
-  })
-});
+createConnection().then(async connection => {
 
-server.listen().then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
-});
+  console.log("Inserting a new user into the database...");
+  const user = new User();
+  user.firstName = "Timber";
+  user.lastName = "Saw";
+  user.age = 25;
+  await connection.manager.save(user);
+  console.log("Saved a new user with id: " + user.id);
+
+  console.log("Loading users from the database...");
+  const users = await connection.manager.find(User);
+  console.log("Loaded users: ", users);
+
+  console.log("Here you can setup and run express/koa/any other framework.");
+  const server = new ApolloServer({
+    typeDefs: gql(fs.readFileSync(__dirname.concat('/schema.graphql'), 'utf8')),
+    resolvers,
+    context: request => ({
+      ...request
+    })
+  });
+  
+  server.listen().then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`);
+  });
+  
+}).catch(error => console.log(error));
+
